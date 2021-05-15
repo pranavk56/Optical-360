ee346071d8b4d0c0b2d7fee06ef75649bcf52e63
Ahir <karan.ahir@accenture.com>
2021-05-15 14:20:08 +0530

    added responsiveness to title

--- src/Components/Home/home.component.jsx
+++ src/Components/Home/home.component.jsx
@@ -4,7 +4,7 @@ export default function Home() {
 
     return (
         <div className="home">
-            <h1 className="header"><img src="LogoFinal2.png" alt="1" height="100" width="100"/><sup>ptical 360</sup><sup>°</sup></h1>
+            <h1 className="header"><img src="LogoFinal2.png" alt="1"/><sup>ptical 360</sup><sup>°</sup></h1>
             <div className="image-container">
                 <div id="image-div-1" className="image-div">
                     <img src="spec5.jpg" alt="1"/>
